58795b1b6b01241b0e19522e9eaf96999cb3c497
Si Parker <caffeinate@users.noreply.github.com>
2026-06-18 15:20:25 +0100

    Field's with 'required' should contain a value (#14)
    * updated: AbstractSchemaField with optional 'required' field which is checked by field level .valid_update()
    * updated: StringField to also check for empty strings

--- tests/test_build_schema.py
+++ tests/test_build_schema.py
@@ -32,7 +32,7 @@ class TestBuildSchema(unittest.TestCase):
             ("class SiteLocation(SchemaNode):", "Descendant of SiteDetails"),
             ("class SiteDetails(SchemaNode):", "Parent node passed to render function"),
             (
-                'site_locations = RepeatedField(schema_field=SchemaNodeField(ref="site-locations"',
+                'site_locations = RepeatedField(required=True, schema_field=SchemaNodeField(ref="site-locations"',
                 "SiteDetails field describing a descendant",
             ),
             ("schema_node_cls=SiteLocation", "Link to child class"),
