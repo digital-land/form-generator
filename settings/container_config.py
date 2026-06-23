e765bedcae7331fe071cfba4de4be19585532d32
Si Parker <si@plogic.co.uk>
2026-06-23 07:17:43 +0100

    updated: CSRF key not needed yet, CSRF middleware left in place.

--- settings/container_config.py
+++ settings/container_config.py
@@ -4,3 +4,5 @@ from settings.global_config import BaseConfig
 class Config(BaseConfig):
     DEBUG = False
     WTF_CSRF_ENABLED = True
+    # CSRF is in place but fully public open data without auth so not actually needed yet
+    SECRET_KEY = "not-actually-secret"
