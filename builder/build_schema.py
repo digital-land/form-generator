2acab969bbba597db91c4b749199e930c338fefb
Si Parker <caffeinate@users.noreply.github.com>
2026-09-15 15:07:26 +0100

    Validator errors need module and field paths (#38)
    updated: error messages to include node paths along with field names
    refactored: builder.build_schema.build_rules into builder.build_conditions.BuildConditions
    simplified: ContraintRule to not include subject field but instead use conjunction with second rule for subject field.
    fixed: missing subject field evaluation for 'any' and 'all' value cases. This was causing the confusing error detailed in https://github.com/digital-land/form-generator/issues/33
    added: SchemaNode.is_empty_field and .node_path which are now used in .valid_node checks

--- builder/build_schema.py
+++ builder/build_schema.py
@@ -1,10 +1,11 @@
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 from functools import cached_property
 import warnings
 
 from jinja2 import Environment, FileSystemLoader
 
 from builder import safe_literal
+from builder.build_conditions import BuildConditions
 from builder.planning_app_data_spec import (
     ComponentResolved,
     Field,
@@ -77,205 +78,6 @@ def walk_resolved_schema(schema_items):
         yield spec_item
 
 
-# Structured encoding of 'required-if' type rules from the specification.
-# in pseudo-code, read this as ...
-#
-# if value_of(switch_field) == switch_value and not value_of(subject_field):
-#     .. validation fails
-#
-# 'switch_method_call' is an optional method on switch_field
-#
-# @see builder/form_templates/schema_tree_class.py.j2 for how this is used
-ContraintRule = namedtuple(
-    "ContraintRule",
-    ("switch_field", "switch_value", "subject_field", "operand", "switch_method_call"),
-    defaults=("",),
-)
-
-
-# Operand rule logic
-class RulesOpLogic:
-    # python boolean operator used to join the contained rules in generated code
-    op = None
-
-    def __init__(self, rule_message, *rules):
-        self.rule_message = rule_message
-        self.rules = rules
-
-
-class RuleDisjunction(RulesOpLogic):
-    "OR"
-
-    op = "or"
-
-
-class RuleConjunction(RulesOpLogic):
-    "AND"
-
-    op = "and"
-
-
-def build_rules(field_x):
-    """
-    Informal structure whilst code is taking shape to specify arguments
-    for template to build validation rules based on field values elsewhere
-    in the schema node tree.
-
-    @param field_x: obj with 'required*' attribs
-
-    @return list of tuple rules.
-    """
-    field_rules = []
-
-    if field_x.required_if:
-        # Used to build SchemaNode.valid_node method in template
-
-        if not isinstance(field_x.required_if, list):
-            if SHOW_WARNINGS:
-                msg = f"Can't build rule for {field_x.ref} - required_if isn't a list"
-                warnings.warn(msg)
-
-            return []
-
-        for rule in field_x.required_if:
-            # if bool rule
-
-            if "value" in rule:
-                # 'if' rule
-                v = safe_literal(rule["value"])
-
-                r = ContraintRule(
-                    switch_field=rule["field"],
-                    switch_value=v,
-                    subject_field=field_x.ref,
-                    operand="==",
-                )
-                field_rules.append(r)
-
-            elif "in" in rule:
-                # member of list
-                members = [safe_literal(v) for v in rule["in"]]
-                m = ", ".join(members)
-                v = f"[{m}]"
-                r = ContraintRule(
-                    switch_field=rule["field"],
-                    switch_value=v,
-                    subject_field=field_x.ref,
-                    operand="in",
-                )
-                field_rules.append(r)
-
-            elif "operator" in rule:
-
-                if rule["operator"] in {"empty", "not_empty"}:
-
-                    if rule["operator"] == "not_empty":
-                        op_empty = ">"
-                    else:
-                        op_empty = "=="
-
-                    # empty list or string
-                    # using conjunction so the field's value works with len(), e.g. string or list
-
-                    r_none = ContraintRule(
-                        switch_field=rule["field"],
-                        switch_value="None",
-                        subject_field=field_x.ref,
-                        operand="is not",
-                    )
-                    r_len = ContraintRule(
-                        switch_field=rule["field"],
-                        switch_method_call=".__len__()",
-                        switch_value=0,
-                        subject_field=field_x.ref,
-                        operand=op_empty,
-                    )
-                    msg = f"Field validation problem for: {rule['field']}"
-                    r_op = RuleConjunction(msg, r_none, r_len)
-                    field_rules.append(r_op)
-
-                else:
-                    if SHOW_WARNINGS:
-                        o = rule["operator"]
-                        msg = f"Can't build rule for {field_x.ref} - unknown operator {o}"
-                        warnings.warn(msg)
-                    continue
-
-            elif "any" in rule or "all" in rule:
-
-                logical_op = "any" if "any" in rule else "all"
-
-                equality_rules = []
-
-                if not isinstance(rule[logical_op], list):
-                    if SHOW_WARNINGS:
-                        msg = f"Can't build rule for {field_x.ref} - '{logical_op}' field isn't a list"
-                        warnings.warn(msg)
-
-                    # this continue would skip any other rules built for this field but I think
-                    # that's OK as this is malformed
-                    continue
-
-                for field_eq in rule[logical_op]:
-
-                    if "field" in field_eq:
-
-                        if "value" in field_eq:
-                            # this 'any' rule is for a field level equality
-                            v = safe_literal(field_eq["value"])
-                            r = ContraintRule(
-                                switch_field=field_eq["field"],
-                                switch_value=v,
-                                subject_field=None,
-                                operand="==",
-                            )
-                            equality_rules.append(r)
-
-                        elif "contains" in field_eq:
-
-                            v = safe_literal(field_eq["contains"])
-                            v_members = f"[{v}]"
-                            r = ContraintRule(
-                                switch_field=field_eq["field"],
-                                switch_value=v_members,
-                                subject_field=field_x.ref,
-                                operand="in",
-                            )
-                            equality_rules.append(r)
-
-                        elif SHOW_WARNINGS:
-                            msg = f"'any' rule without 'container' or 'value' for {field_x.ref}"
-                            warnings.warn(msg)
-
-                    elif SHOW_WARNINGS:
-                        msg = f"Can't build rule for {field_x.ref} - unknown 'any' rule"
-                        warnings.warn(msg)
-
-                if len(equality_rules) > 0:
-                    deduped_fields = list(set([r.switch_field for r in equality_rules]))
-                    # deterministic order for tests
-                    deduped_fields.sort()
-                    msg_fields = ", ".join(deduped_fields)
-
-                    if logical_op == "any":
-                        msg = f"One or more matches required in field(s): {msg_fields}"
-                        r_op = RuleDisjunction(msg, *equality_rules)
-                    elif logical_op == "all":
-                        msg = f"All fields need to match for field(s): {msg_fields}"
-                        r_op = RuleConjunction(msg, *equality_rules)
-                    else:
-                        raise ValueError("Unknown logical operation when building rules.")
-
-                    field_rules.append(r_op)
-
-            else:
-                if SHOW_WARNINGS:
-                    warnings.warn(f"Can't build rule for {field_x.ref}")
-                continue
-
-    return field_rules
-
-
 def render_python(project_root, planning_spec):
     """
     @param project_root: (str)
@@ -285,7 +87,8 @@ def render_python(project_root, planning_spec):
     """
 
     render = TemplatedBuilder(project_root=project_root)
-    py_output = render.build(dict(document_header=True), "schema_tree_class.py.j2")
+    template = "schema_tree_class.py.j2"
+    py_output = render.build(dict(document_header=True), template)
 
     # assumption - refs are primary keys
     segment_register = defaultdict(dict)
@@ -346,7 +149,7 @@ def render_python(project_root, planning_spec):
 
             elif isinstance(field_x, Field):
 
-                field_rules = build_rules(field_x)
+                field_rules = BuildConditions.rules(field_x)
                 validation_simplified.extend(field_rules)
 
                 if field_x.datatype == "string":
@@ -388,7 +191,7 @@ def render_python(project_root, planning_spec):
 
             # module + component rules
             if field_entry.origin != field_entry.target:
-                field_rules = build_rules(field_entry.origin)
+                field_rules = BuildConditions.rules(field_entry.origin)
                 validation_simplified.extend(field_rules)
 
             applies_if = getattr(field_entry.origin, "applies_if")
@@ -452,9 +255,9 @@ def render_python(project_root, planning_spec):
             "validation_rules": validation_simplified,
             "out_of_scope_rules": out_of_scope_rules,
         }
-        py_output += render.build(template_context, "schema_tree_class.py.j2")
+        py_output += render.build(template_context, template)
 
-    py_output += render.build(dict(document_footer=True), "schema_tree_class.py.j2")
+    py_output += render.build(dict(document_footer=True), template)
 
     return py_output
 
--- tests/test_schema_planning.py
+++ tests/test_schema_planning.py
@@ -30,10 +30,10 @@ class TestSchemaPlanning(unittest.TestCase):
         payload = {"advice-sought": True}
 
         expected = [
-            "officer-name is needed for current value in 'advice-sought'",
-            "reference is needed for current value in 'advice-sought'",
-            "advice-date is needed for current value in 'advice-sought'",
-            "advice-summary is needed for current value in 'advice-sought'",
+            ".officer-name is needed for current value in .advice-sought",
+            ".reference is needed for current value in .advice-sought",
+            ".advice-date is needed for current value in .advice-sought",
+            ".advice-summary is needed for current value in .advice-sought",
         ]
 
         node = PreAppAdvice()
@@ -90,7 +90,7 @@ class TestSchemaPlanning(unittest.TestCase):
         Field is required when another field is one of several values.
         """
         payload = {"contact-type": "agent"}
-        expected = "contact-reference is needed for current value in 'contact-type'"
+        expected = ".contact-reference is needed for current value in .contact-type"
 
         node = SiteVisit()
         with self.assertRaises(SchemaValidationException) as ctx:
@@ -106,7 +106,7 @@ class TestSchemaPlanning(unittest.TestCase):
         """
 
         payload = {"use": "sui"}
-        expected = "One or more matches required in field(s): use"
+        expected = "One or more matches required for  in field(s): use"
 
         node = FloorspaceDetails()
         with self.assertRaises(SchemaValidationException) as ctx:
@@ -132,8 +132,8 @@ class TestSchemaPlanning(unittest.TestCase):
             ],
         }
 
-        payload = {"operational-times": [], "hours-not-known": False}
-        target_reason = "Field validation problem for: operational-times"
+        payload = {"operational-times": [], "hours-not-known": True}
+        target_reason = ".operational-times is needed for current value in .hours-not-known"
 
         node = HoursOfOperation()
         with self.assertRaises(SchemaValidationException) as ctx:
@@ -142,7 +142,7 @@ class TestSchemaPlanning(unittest.TestCase):
         self.assertIn(target_reason, ctx.exception.reasons)
 
         msg = "Non-empty operational-times is valid"
-        payload = {"operational-times": [operational_times_sample], "hours-not-known": False}
+        payload["operational-times"].append(operational_times_sample)
 
         failure_reasons = []
         try:
